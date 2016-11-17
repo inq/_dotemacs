dae28d7870b202fb29b4fec49676634a16a56569
Inkyu Lee <inkyu@prestolabs.io>
2016-11-17 17:14:28 +0800

    update: 20161117

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,15 +1,22 @@
-;;; Code: init.el
+;;; init.el --- The entry point.
+;; inkyu, 2012.
+
+;;; Commentary:
+
+;;; Code:
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+;; RUST
 (add-hook 'rust-mode-hook #'racer-mode)
 (add-hook 'racer-mode-hook #'eldoc-mode)
 (add-hook 'racer-mode-hook #'company-mode)
 (add-hook 'haskell-mode-hook #'company-mode)
-(setq racer-cmd "~/.cargo/bin/racer")
-(setq racer-rust-src-path "~/dev/rustc-1.8.0/src/")
+(custom-set-variables
+ '(racer-cmd "~/.cargo/bin/racer")
+ '(racer-rust-src-path  "~/dev/rustc-1.8.0/src/"))
 
 (defun complete-or-indent ()
   (interactive)
@@ -18,15 +25,22 @@
           (indent-according-to-mode)))
 
 (global-set-key (kbd "TAB") #'complete-or-indent) ;
-(setq company-tooltip-align-annotations t)
 
-(global-linum-mode t)
-(setq linum-format "%4d \u2502 ")
+;; COMPANY
+(custom-set-variables
+ '(company-tooltip-align-annotations t)
+ '(company-idle-delay 0)
+ '(company-minimum-prefix-length 1))
 
-(setq company-idle-delay 0)
-(setq company-minimum-prefix-length 1)
+;; LINE NUMBERS
+(global-nlinum-mode t)
+(custom-set-variables '(nlinum-format "%3d "))
+(custom-set-faces
+ `(linum ((t (:inherit (shadow default)
+                       :background "#303030"
+                       :foreground "white")))))
 
-(mapcar #'(lambda (arg) (apply #'set-face-attribute arg))
+(dolist #'(lambda (arg) (apply #'set-face-attribute arg))
         '((default nil :family "Monaco"
             :foreground "powder blue" :background "black")
           (isearch nil :foreground "brown4" :background "palevioletred2")
@@ -45,22 +59,26 @@
 (global-flycheck-mode)
 
 
-(setq indent-tabs-mode nil)
-(setq python-indent-offset 2)
+(setq-default indent-tabs-mode nil)
+(setq-default python-indent-offset 2)
 (add-hook 'python-mode-hook
           (lambda ()
             (fci-mode)
-            (setq tab-width 2)
-            (setq python-indent 2)))
+            (custom-set-variables
+             '(tab-width 2)
+             '(python-indent 2))))
 
 (autoload 'pylint "pylint")
 (add-hook 'python-mode-hook 'pylint-add-menu-items)
 (add-hook 'python-mode-hook 'pylint-add-key-bindings)
 
 
+(require 'ede)
+(global-ede-mode)
+
 ; TRAILING LINES
 (defun delete-trailing-blank-lines ()
-  "Deletes all blank lines at the end of the file, even the last one"
+  "Deletes all blank lines at the end of the file, even the last one."
   (interactive)
   (save-excursion
     (save-restriction
@@ -91,9 +109,8 @@
 
 (require 'highlight-chars)
 (require 'fill-column-indicator)
+(setq-default fci-rule-column 80)
 
-;(providd 'init)
-;;; init.el ends here
 
 (require 'magit-gerrit)
 (setq-default magit-gerrit-remote "gerrit")
@@ -102,3 +119,40 @@
 (global-set-key (kbd "C-c m") 'magit-status)
 (require 'magit-gitflow)
 (add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
+
+;; W3M
+(setq browse-url-browser-function 'w3m-browse-url)
+
+;; ELPY
+(elpy-enable)
+(global-set-key (kbd "M-]") 'elpy-goto-definition)
+(global-set-key (kbd "M-[") 'pop-tag-mark)
+(setq elpy-syntax-check-command 'pylint)
+
+;; RTAGS
+(require 'rtags)
+(setq rtags-autostart-diagnostics t)
+(rtags-diagnostics)
+(setq rtags-completions-enabled t)
+(push 'company-rtags company-backends)
+(global-company-mode)
+(define-key c-mode-base-map (kbd "<C-tab>") (function company-complete))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ede-project-directories (quote ("/home/inkyu/dev/navi")))
+ '(package-selected-packages
+   (quote
+    (nlinum yaml-mode w3m rtags racer python-mode pylint magit-gitflow magit-gerrit json-mode hlinum highlight-chars google-c-style flycheck-rust flycheck-haskell fish-mode fill-column-indicator elpy ebal company-ghc column-marker ac-haskell-process))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+(provide 'init)
+;;; init.el ends here
