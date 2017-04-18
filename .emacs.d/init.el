63fd2f1c54f555ddc9a5b3ef7ebb930790333fe3
Inkyu Lee <inkyu@prestolabs.io>
2017-04-19 01:27:56 +0800

    csv-mode, protobuf-mod

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -33,13 +33,21 @@
 (require 'rtags-helm)
 (setq rtags-use-helm t)
 
+;; NLINUM
+(global-nlinum-mode)
+
+;; C++
+(add-hook 'c-mode-common-hook 'google-set-c-style)
+(add-hook 'c-mode-common-hook 'google-make-newline-indent)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (haskell-mode racer rust-mode nlinum helm rtags))))
+ '(package-selected-packages
+   (quote
+    (fill-column-indicator google-c-style protobuf-mode csv-mode haskell-mode racer rust-mode nlinum helm rtags))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
