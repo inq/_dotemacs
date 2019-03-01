d5ffaa63cb628a582e01e221387af4014cc455e9
Inkyu Lee <inkyu@hive-61.iosg.corp.prestolabs.io>
2019-03-01 15:55:33 +0800

    Update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -44,7 +44,12 @@
 (setq-default helm-M-x-fuzzy-match t)
 (setq-default helm-completion-in-region-fuzzy-match t)
 
-(global-set-key (kbd "C-x C-f") #'fiplr-find-file)
+
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+(global-set-key (kbd "C-x C-f") #'helm-projectile)
 
 (setq fiplr-ignored-globs '((directories (".git" ".svn" "venv"))
                             (files ("*.jpg" "*.png" "*.zip" "*~"))))
@@ -60,7 +65,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (git-gutter fiplr elpy helm-fuzzy-find helm-fuzzier helm nlinum fill-column-indicator))))
+    (rust-mode helm-projectile projectile git-gutter elpy helm-fuzzy-find helm-fuzzier helm nlinum fill-column-indicator))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
