8d14f3ceb9e039ad42e0215c1d65bcb5bd733b62
Inkyu Lee <inkyu@hive-61.iosg.corp.prestolabs.io>
2019-05-13 18:37:28 +0800

    Update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -32,6 +32,16 @@
 (setq fci-rule-character ?┃)
 (global-fci-mode 1)
 
+;; RUST
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hool #'eldoc-mode)
+
+(add-hook 'racer-mode-hook #'company-mode)
+
+(require 'rust-mode)
+(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+(setq company-tooltip-align-annotations t)
+
 ;; HELM
 (require 'helm-config)
 (global-set-key (kbd "M-x") #'helm-M-x)
@@ -51,12 +61,17 @@
 
 (global-set-key (kbd "C-x C-f") #'helm-projectile)
 
-(setq fiplr-ignored-globs '((directories (".git" ".svn" "venv"))
-                            (files ("*.jpg" "*.png" "*.zip" "*~"))))
-
 (helm-mode 1)
 
 (define-key helm-find-files-map "\t" 'helm-execute-persistent-action)
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+(with-eval-after-load 'rust-mode
+  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
+(with-eval-after-load 'flycheck
+		        (add-hook 'flycheck-mode-hook #'flycheck-inline-mode))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -65,7 +80,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (rust-mode helm-projectile projectile git-gutter elpy helm-fuzzy-find helm-fuzzier helm nlinum fill-column-indicator))))
+    (racer flycheck-inline flycheck-rust rust-mode helm-projectile projectile git-gutter elpy helm-fuzzy-find helm-fuzzier helm nlinum fill-column-indicator))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
